4f8f50f8b5072989290e339cd322f51fd19eb3c2
sasaco <sasaicco@gmail.com>
2021-11-29 07:22:03 +0900

    tab4

--- src/Lanczos.py
+++ src/Lanczos.py
@@ -9,42 +9,42 @@ EIG_SHIFT=-0.1	# シフト量δのデフォルト値
 # a,b - 行列A,B
 # n - ３重対角化行列の大きさ
 def eigByLanczos(a,b,n):
-	size=len(a)
-	u=[]
-	alp=[]
-	bet=[]
-	n=min(n, size)
-	blup=numeric.ccsLUP(numeric.ccsSparse(b))
-	u[0]=[]
-	for i in range(size):
-		u[0][i]=a[i][i]
-	a=toSparse(a)
-	b=toSparse(b)
-	ubu=sparseDotVMV(b,u[0])
-	u[0]=numeric.mul(1/math.sqrt(abs(ubu)),u[0])
-	for i in range(n-1):
-		al=sparseDotVMV(a,u[i])
-		alp.append(al)
-		bau=numeric.ccsLUPSolve(blup,sparseDotMV(a,u[i]))
-		v=numeric.sub(bau,numeric.mul(al,u[i]))
-		if i>0:
-			v=numeric.sub(v,numeric.mul(bt,u[i-1]))
-		for j in range(i+1):
-			uv=numeric.dot(u[j],sparseDotMV(b,v))
-			v=numeric.sub(v,numeric.mul(uv,u[j]))
+    size=len(a)
+    u=[]
+    alp=[]
+    bet=[]
+    n=min(n, size)
+    blup=numeric.ccsLUP(numeric.ccsSparse(b))
+    u[0]=[]
+    for i in range(size):
+        u[0][i]=a[i][i]
+    a=toSparse(a)
+    b=toSparse(b)
+    ubu=sparseDotVMV(b,u[0])
+    u[0]=numeric.mul(1/math.sqrt(abs(ubu)),u[0])
+    for i in range(n-1):
+        al=sparseDotVMV(a,u[i])
+        alp.append(al)
+        bau=numeric.ccsLUPSolve(blup,sparseDotMV(a,u[i]))
+        v=numeric.sub(bau,numeric.mul(al,u[i]))
+        if i>0:
+            v=numeric.sub(v,numeric.mul(bt,u[i-1]))
+        for j in range(i+1):
+            uv=numeric.dot(u[j],sparseDotMV(b,v))
+            v=numeric.sub(v,numeric.mul(uv,u[j]))
 
-		ubu=sparseDotVMV(b,v)
-		bt=math.sqrt(abs(ubu))
-		bet.append(bt)
-		u[i+1]=numeric.mul(1/bt,v)
+        ubu=sparseDotVMV(b,v)
+        bt=math.sqrt(abs(ubu))
+        bet.append(bt)
+        u[i+1]=numeric.mul(1/bt,v)
 
-	al=sparseDotVMV(a,u[n-1])
-	alp.append(al)
-	eig=tdmEig(alp,bet)
-	return {
-		"lambda":eig[0],
-		"ut":numeric.dot(eig[1],u)
-	}
+    al=sparseDotVMV(a,u[n-1])
+    alp.append(al)
+    eig=tdmEig(alp,bet)
+    return {
+        "lambda":eig[0],
+        "ut":numeric.dot(eig[1],u)
+    }
 
 
 # ランチョス法で一般固有値問題Ax=λBxの３重対角化をする
@@ -54,48 +54,48 @@ def eigByLanczos(a,b,n):
 # n - ３重対角化行列の大きさ
 # delta - シフト量δ
 def eigByLanczosLUP(a,b,n,delta):
-	size=len(a)
-	u=[]
-	alp=[]
-	bet=[]
-	n=min(n,size)
-	if delta==None:
-		delta=EIG_SHIFT
-	ablup=numeric.ccsLUP(numeric.ccsSparse(shiftMatrix(a,b,delta)))
-	b=toSparse(b)
-	u[0]=[]
-	for i in range(size):
-		u[0][i]=a[i][i]
-	ubu=sparseDotVMV(b,u[0])
-	u[0]=numeric.mul(1/math.sqrt(abs(ubu)),u[0])
-	for i in range(n):
-		bu=sparseDotMV(b,u[i])
-		abu=numeric.ccsLUPSolve(ablup,bu)
-		al=numeric.dot(bu,abu)
-		alp.append(al)
-		if i < n-1:
-			v=numeric.sub(abu,numeric.mul(al,u[i]))
-			if i>0:
-				v=numeric.sub(v,numeric.mul(bt,u[i-1]))
-			for j in range(i+1):
-				uv=numeric.dot(u[j],sparseDotMV(b,v))
-				v=numeric.sub(v,numeric.mul(uv,u[j]))
+    size=len(a)
+    u=[]
+    alp=[]
+    bet=[]
+    n=min(n,size)
+    if delta==None:
+        delta=EIG_SHIFT
+    ablup=numeric.ccsLUP(numeric.ccsSparse(shiftMatrix(a,b,delta)))
+    b=toSparse(b)
+    u[0]=[]
+    for i in range(size):
+        u[0][i]=a[i][i]
+    ubu=sparseDotVMV(b,u[0])
+    u[0]=numeric.mul(1/math.sqrt(abs(ubu)),u[0])
+    for i in range(n):
+        bu=sparseDotMV(b,u[i])
+        abu=numeric.ccsLUPSolve(ablup,bu)
+        al=numeric.dot(bu,abu)
+        alp.append(al)
+        if i < n-1:
+            v=numeric.sub(abu,numeric.mul(al,u[i]))
+            if i>0:
+                v=numeric.sub(v,numeric.mul(bt,u[i-1]))
+            for j in range(i+1):
+                uv=numeric.dot(u[j],sparseDotMV(b,v))
+                v=numeric.sub(v,numeric.mul(uv,u[j]))
 
-			ubu=sparseDotVMV(b,v)
-			bt=math.sqrt(abs(ubu))
-			bet.append(bt)
-			u[i+1]=numeric.mul(1/bt,v)
+            ubu=sparseDotVMV(b,v)
+            bt=math.sqrt(abs(ubu))
+            bet.append(bt)
+            u[i+1]=numeric.mul(1/bt,v)
 
-	eig=tdmEig(alp,bet)
-	e1=eig[0]
-	e2=[]
-	for i in range(len(e1)):
-		e2[i]=1/e1[i]+delta
+    eig=tdmEig(alp,bet)
+    e1=eig[0]
+    e2=[]
+    for i in range(len(e1)):
+        e2[i]=1/e1[i]+delta
 
-	return {
-		"lambda":e2,
-		"ut":numeric.dot(eig[1],u)
-	}
+    return {
+        "lambda":e2,
+        "ut":numeric.dot(eig[1],u)
+    }
 
 
 # ランチョス法で一般固有値問題Ax=λBxの３重対角化をする
@@ -105,50 +105,50 @@ def eigByLanczosLUP(a,b,n,delta):
 # n - ３重対角化行列の大きさ
 # delta - シフト量δ
 def eigByLanczosILUCG(a,b,n,delta):
-	size=len(a)
-	u=[]
-	alp=[]
-	bet=[]
-	n=min(n,size)
-	if delta==None:
-		delta=EIG_SHIFT
-	adb=shiftMatrix(a,b,delta)
-	ablu=getILU(adb)
-	adb=toSparse(adb)
-	b=toSparse(b)
-	u[0]=[]
-	for i in range(size):
-		u[0][i]=a[i][i]
-	ubu=sparseDotVMV(b,u[0])
-	u[0]=numeric.mul(1/math.sqrt(abs(ubu)),u[0])
-	for i in range(n):
-		bu=sparseDotMV(b,u[i])
-		abu=solveILU(adb,ablu,bu)
-		al=numeric.dot(bu,abu)
-		alp.append(al)
-		if i<n-1:
-			v=numeric.sub(abu,numeric.mul(al,u[i]))
-			if i>0:
-				v=numeric.sub(v,numeric.mul(bt,u[i-1]))
-			for j in range(i+1):
-				uv=numeric.dot(u[j],sparseDotMV(b,v))
-				v=numeric.sub(v,numeric.mul(uv,u[j]))
+    size=len(a)
+    u=[]
+    alp=[]
+    bet=[]
+    n=min(n,size)
+    if delta==None:
+        delta=EIG_SHIFT
+    adb=shiftMatrix(a,b,delta)
+    ablu=getILU(adb)
+    adb=toSparse(adb)
+    b=toSparse(b)
+    u[0]=[]
+    for i in range(size):
+        u[0][i]=a[i][i]
+    ubu=sparseDotVMV(b,u[0])
+    u[0]=numeric.mul(1/math.sqrt(abs(ubu)),u[0])
+    for i in range(n):
+        bu=sparseDotMV(b,u[i])
+        abu=solveILU(adb,ablu,bu)
+        al=numeric.dot(bu,abu)
+        alp.append(al)
+        if i<n-1:
+            v=numeric.sub(abu,numeric.mul(al,u[i]))
+            if i>0:
+                v=numeric.sub(v,numeric.mul(bt,u[i-1]))
+            for j in range(i+1):
+                uv=numeric.dot(u[j],sparseDotMV(b,v))
+                v=numeric.sub(v,numeric.mul(uv,u[j]))
 
-			ubu=sparseDotVMV(b,v)
-			bt=math.sqrt(abs(ubu))
-			bet.append(bt)
-			u[i+1]=numeric.mul(1/bt,v)
+            ubu=sparseDotVMV(b,v)
+            bt=math.sqrt(abs(ubu))
+            bet.append(bt)
+            u[i+1]=numeric.mul(1/bt,v)
 
-	eig=tdmEig(alp,bet)
-	e1=eig[0]
-	e2=[]
-	for i in range(len(e1)):
-		e2[i]=1/e1[i]+delta
+    eig=tdmEig(alp,bet)
+    e1=eig[0]
+    e2=[]
+    for i in range(len(e1)):
+        e2[i]=1/e1[i]+delta
 
-	return {
-		"lambda":e2,
-		"ut":numeric.dot(eig[1],u)
-	}
+    return {
+        "lambda":e2,
+        "ut":numeric.dot(eig[1],u)
+    }
 
 
 # アーノルディ法で一般固有値問題Ax=λBxの３重対角化をする
@@ -158,47 +158,47 @@ def eigByLanczosILUCG(a,b,n,delta):
 # n - ３重対角化行列の大きさ
 # delta - シフト量δ
 def eigByArnoldiLUP(a,b,n,delta):
-	size=len(a)
-	u=[]
-	n=min(n,size)
-	h=numeric.rep([n,n],0)
-	if delta==None:
-		delta=EIG_SHIFT
-	ablup=numeric.ccsLUP(numeric.ccsSparse(shiftMatrix(a,b,delta)))
-	b=toSparse(b)
-	u[0]=[]
-	for i in range(size):
-		u[0][i]=a[i][i]
-	uu=numeric.dotVV(u[0],u[0])
-	u[0]=numeric.mul(1/math.sqrt(uu),u[0])
-	for i in range(n):
-		bu=sparseDotMV(b,u[i])
-		abu=numeric.ccsLUPSolve(ablup,bu)
-		v=abu
-		for j in range(i+1):
-			h1=numeric.dot(u[j],abu)
-			h[j][i]=h1
-			v=numeric.sub(v,numeric.mul(h1,u[j]))
+    size=len(a)
+    u=[]
+    n=min(n,size)
+    h=numeric.rep([n,n],0)
+    if delta==None:
+        delta=EIG_SHIFT
+    ablup=numeric.ccsLUP(numeric.ccsSparse(shiftMatrix(a,b,delta)))
+    b=toSparse(b)
+    u[0]=[]
+    for i in range(size):
+        u[0][i]=a[i][i]
+    uu=numeric.dotVV(u[0],u[0])
+    u[0]=numeric.mul(1/math.sqrt(uu),u[0])
+    for i in range(n):
+        bu=sparseDotMV(b,u[i])
+        abu=numeric.ccsLUPSolve(ablup,bu)
+        v=abu
+        for j in range(i+1):
+            h1=numeric.dot(u[j],abu)
+            h[j][i]=h1
+            v=numeric.sub(v,numeric.mul(h1,u[j]))
 
-		if i<n-1:
-			for j in range(i+1):
-				uv=numeric.dot(u[j],v)
-				v=numeric.sub(v,numeric.mul(uv,u[j]))
+        if i<n-1:
+            for j in range(i+1):
+                uv=numeric.dot(u[j],v)
+                v=numeric.sub(v,numeric.mul(uv,u[j]))
 
-			uu=numeric.dotVV(v,v)
-			h1=math.sqrt(uu)
-			h[i+1][i]=h1
-			u[i+1]=numeric.mul(1/h1,v)
+            uu=numeric.dotVV(v,v)
+            h1=math.sqrt(uu)
+            h[i+1][i]=h1
+            u[i+1]=numeric.mul(1/h1,v)
 
-	eig=eigen(h)
-	e1=eig[0]
-	e2=[]
-	for i in range(len(e1)):
-		e2[i]=1/e1[i]+delta
-	return {
-		"lambda":e2,
-		"ut":numeric.dot(eig[1],u)
-	}
+    eig=eigen(h)
+    e1=eig[0]
+    e2=[]
+    for i in range(len(e1)):
+        e2[i]=1/e1[i]+delta
+    return {
+        "lambda":e2,
+        "ut":numeric.dot(eig[1],u)
+    }
 
 # アーノルディ法で一般固有値問題Ax=λBxの３重対角化をする
 # 小さい固有値を優先させるため (A+δB)^-1Bx=θx に変換する
@@ -207,126 +207,126 @@ def eigByArnoldiLUP(a,b,n,delta):
 # n - ３重対角化行列の大きさ
 # delta - シフト量δ
 def eigByArnoldiILUCG(a,b,n,delta):
-	size=len(a)
-	u=[]
-	n=min(n,size)
-	h=numeric.rep([n,n],0)
-	if delta==None:
-		delta=EIG_SHIFT
-	adb=shiftMatrix(a,b,delta)
-	ablu=getILU(adb)
-	adb=toSparse(adb)
-	b=toSparse(b)
-	u[0]=[]
-	for i in range(size):
-		u[0][i]=a[i][i]
-	uu=numeric.dotVV(u[0],u[0])
-	u[0]=numeric.mul(1/math.sqrt(uu),u[0])
-	for i in range(n):
-		bu=sparseDotMV(b,u[i])
-		abu=solveILU(adb,ablu,bu)
-		v=abu
-		for j in range(i+1):
-			h1=numeric.dot(u[j],abu)
-			h[j][i]=h1
-			v=numeric.sub(v,numeric.mul(h1,u[j]))
+    size=len(a)
+    u=[]
+    n=min(n,size)
+    h=numeric.rep([n,n],0)
+    if delta==None:
+        delta=EIG_SHIFT
+    adb=shiftMatrix(a,b,delta)
+    ablu=getILU(adb)
+    adb=toSparse(adb)
+    b=toSparse(b)
+    u[0]=[]
+    for i in range(size):
+        u[0][i]=a[i][i]
+    uu=numeric.dotVV(u[0],u[0])
+    u[0]=numeric.mul(1/math.sqrt(uu),u[0])
+    for i in range(n):
+        bu=sparseDotMV(b,u[i])
+        abu=solveILU(adb,ablu,bu)
+        v=abu
+        for j in range(i+1):
+            h1=numeric.dot(u[j],abu)
+            h[j][i]=h1
+            v=numeric.sub(v,numeric.mul(h1,u[j]))
 
-		if i<n-1:
-			for j in range(i+1):
-				uv=numeric.dot(u[j],v)
-				v=numeric.sub(v,numeric.mul(uv,u[j]))
+        if i<n-1:
+            for j in range(i+1):
+                uv=numeric.dot(u[j],v)
+                v=numeric.sub(v,numeric.mul(uv,u[j]))
 
-			uu=numeric.dotVV(v,v)
-			h1=math.sqrt(uu)
-			h[i+1][i]=h1
-			u[i+1]=numeric.mul(1/h1,v)
+            uu=numeric.dotVV(v,v)
+            h1=math.sqrt(uu)
+            h[i+1][i]=h1
+            u[i+1]=numeric.mul(1/h1,v)
 
-	eig=eigen(h)
-	e1=eig[0]
-	e2=[]
-	for i in range(len(e1)):
-		e2[i]=1/e1[i]+delta
-	return {
-		"lambda":e2,
-		"ut":numeric.dot(eig[1],u)
-	}
+    eig=eigen(h)
+    e1=eig[0]
+    e2=[]
+    for i in range(len(e1)):
+        e2[i]=1/e1[i]+delta
+    return {
+        "lambda":e2,
+        "ut":numeric.dot(eig[1],u)
+    }
 
 
 # ３重対角化行列の固有値・固有ベクトルを求める
 # alp,bet - ３重対角化行列の対角成分,その隣の成分
 def tdmEig(alp,bet):
-	size=len(alp)
-	t=numeric.diag(alp)
-	for i in range(size-1):
-		t[i][i+1]=bet[i]
-		t[i+1][i]=bet[i]
+    size=len(alp)
+    t=numeric.diag(alp)
+    for i in range(size-1):
+        t[i][i+1]=bet[i]
+        t[i+1][i]=bet[i]
 
-	return eigen(t)
+    return eigen(t)
 
 
 # 行列の固有値・固有ベクトルを求める
 # m - 行列
 def eigen(m):
-	e=[]
-	ev=[]
-	size=len(m)
-	eig=numeric.eig(m)
-	q=numeric.transpose(eig.E.x)
-	for i in range(size):
-		ev.append([eig['lambda'].x[i],q[i]])
-	ev.sort(function(v1,v2):return abs(v2[0])-abs(v1[0])})
-	for i in range(size):
-		e[i]=ev[i][0]
-		q[i]=ev[i][1]
+    e=[]
+    ev=[]
+    size=len(m)
+    eig=numeric.eig(m)
+    q=numeric.transpose(eig.E.x)
+    for i in range(size):
+        ev.append([eig['lambda'].x[i],q[i]])
+    ev.sort(function(v1,v2):return abs(v2[0])-abs(v1[0])})
+    for i in range(size):
+        e[i]=ev[i][0]
+        q[i]=ev[i][1]
 
-	return [e,q]
+    return [e,q]
 
 
 # シフト行列A-δBを返す
 # a,b - 行列A,B
 # delta - シフト量δ
 def shiftMatrix(a,b,delta):
-	size=len(a)
-	adb=[]
-	for i in range(size):
-		ai=a[i]
-		bi=b[i]
-		s=[]
-		for j in ai:
-			if ai.hasOwnProperty(j):
-				j=int(j)
-				s[j]=ai[j]
+    size=len(a)
+    adb=[]
+    for i in range(size):
+        ai=a[i]
+        bi=b[i]
+        s=[]
+        for j in ai:
+            if ai.hasOwnProperty(j):
+                j=int(j)
+                s[j]=ai[j]
 
-		if delta != 0:
-			for j in bi:
-				if bi.hasOwnProperty(j):
-					j=int(j)
-					if j in s:
-						s[j]-=delta*bi[j]
+        if delta != 0:
+            for j in bi:
+                if bi.hasOwnProperty(j):
+                    j=int(j)
+                    if j in s:
+                        s[j]-=delta*bi[j]
 
-					else:
-						s[j]=-delta*bi[j]
+                    else:
+                        s[j]=-delta*bi[j]
 
-		adb[i]=s
+        adb[i]=s
 
-	return adb
+    return adb
 
 
 # 行列とベクトルの積を計算する
 # matrix - 疎行列
 # x - ベクトル
 def sparseDotVMV(matrix,x):
-	row=matrix[0]
-	col=matrix[1]
-	val=matrix[2]
-	m=len(row)-1
-	s=0
-	for i in range(m):
-		j0=row[i]
-		j1=row[i+1]
-		xi=x[i]
-		for j in range(j0, j1):
-			s+=xi*val[j]*x[col[j]]
+    row=matrix[0]
+    col=matrix[1]
+    val=matrix[2]
+    m=len(row)-1
+    s=0
+    for i in range(m):
+        j0=row[i]
+        j1=row[i+1]
+        xi=x[i]
+        for j in range(j0, j1):
+            s+=xi*val[j]*x[col[j]]
 
-	return s
+    return s
 
--- src/Material.py
+++ src/Material.py
@@ -12,64 +12,64 @@ KS_CIRCLE=6/7			# 円形断面のせん断補正係数
 # hCon - 熱伝導率
 # sHeat - 比熱
 class Material:
-	def __init__(self,label,ee,nu,dens,hCon,sHeat):
-		self.label=label
-		self.ee=ee
-		self.nu=nu
-		self.dens=dens
-		self.hCon=hCon
-		self.sHeat=sHeat
-		self.gg=0.5*ee/(1+nu)	# 横弾性係数
-		self.cv=dens*sHeat		# 体積比熱
-		self.matrix=None		# 応力 - 歪マトリックス
+    def __init__(self,label,ee,nu,dens,hCon,sHeat):
+        self.label=label
+        self.ee=ee
+        self.nu=nu
+        self.dens=dens
+        self.hCon=hCon
+        self.sHeat=sHeat
+        self.gg=0.5*ee/(1+nu)	# 横弾性係数
+        self.cv=dens*sHeat		# 体積比熱
+        self.matrix=None		# 応力 - 歪マトリックス
 
 
-	# 平面応力問題の応力 - 歪マトリックスを作成する
-	def matrix2Dstress(self):
-		coef = self.ee / (1-self.nu*self.nu)
-		return [[coef,coef*self.nu,0],[coef*self.nu,coef,0],[0,0,self.gg]]
+    # 平面応力問題の応力 - 歪マトリックスを作成する
+    def matrix2Dstress(self):
+        coef = self.ee / (1-self.nu*self.nu)
+        return [[coef,coef*self.nu,0],[coef*self.nu,coef,0],[0,0,self.gg]]
 
 
-	# 平面歪問題の応力 - 歪マトリックスを作成する
-	def matrix2Dstrain(self):
-		coef=self.ee/((1+self.nu)*(1-2*self.nu))
-		return [[coef*(1-self.nu),coef*self.nu,0],
-						[coef*self.nu,coef*(1-self.nu),0],[0,0,self.gg]]
+    # 平面歪問題の応力 - 歪マトリックスを作成する
+    def matrix2Dstrain(self):
+        coef=self.ee/((1+self.nu)*(1-2*self.nu))
+        return [[coef*(1-self.nu),coef*self.nu,0],
+                        [coef*self.nu,coef*(1-self.nu),0],[0,0,self.gg]]
 
 
-	# 軸対称問題の応力 - 歪マトリックスを作成する
-	def matrixAxiSymetric(self):
-		coef=self.ee/((1+self.nu)*(1-2*self.nu))
-		s1=coef*(1-self.nu),s2=coef*self.nu
-		return [[s1,s2,s2,0],[s2,s1,s2,0],[s2,s2,s1,0],[0,0,0,self.gg]]
+    # 軸対称問題の応力 - 歪マトリックスを作成する
+    def matrixAxiSymetric(self):
+        coef=self.ee/((1+self.nu)*(1-2*self.nu))
+        s1=coef*(1-self.nu),s2=coef*self.nu
+        return [[s1,s2,s2,0],[s2,s1,s2,0],[s2,s2,s1,0],[0,0,0,self.gg]]
 
 
-	# 捩り問題の応力 - 歪マトリックスを作成する
-	def matrixTorsion(self):
-		return [[self.gg,0],[0,self.gg]]
+    # 捩り問題の応力 - 歪マトリックスを作成する
+    def matrixTorsion(self):
+        return [[self.gg,0],[0,self.gg]]
 
 
-	# 3次元問題の応力 - 歪マトリックスを作成する
-	def matrix3D(self):
-		coef=self.ee/((1+self.nu)*(1-2*self.nu))
-		s1=coef*(1-self.nu),s2=coef*self.nu
-		return [[s1,s2,s2,0,0,0],[s2,s1,s2,0,0,0],[s2,s2,s1,0,0,0],
-						[0,0,0,self.gg,0,0],[0,0,0,0,self.gg,0],[0,0,0,0,0,self.gg]]
+    # 3次元問題の応力 - 歪マトリックスを作成する
+    def matrix3D(self):
+        coef=self.ee/((1+self.nu)*(1-2*self.nu))
+        s1=coef*(1-self.nu),s2=coef*self.nu
+        return [[s1,s2,s2,0,0,0],[s2,s1,s2,0,0,0],[s2,s2,s1,0,0,0],
+                        [0,0,0,self.gg,0,0],[0,0,0,0,self.gg,0],[0,0,0,0,0,self.gg]]
 
 
-	# シェル要素の応力 - 歪マトリックスを作成する
-	def matrixShell(self):
-		coef=self.ee/(1-self.nu*self.nu)
-		s2=coef*self.nu
-		return [[coef,s2,0,0,0],[s2,coef,0,0,0],[0,0,self.gg,0,0],
-						[0,0,0,KS_RECT*self.gg,0],[0,0,0,0,KS_RECT*self.gg]]
+    # シェル要素の応力 - 歪マトリックスを作成する
+    def matrixShell(self):
+        coef=self.ee/(1-self.nu*self.nu)
+        s2=coef*self.nu
+        return [[coef,s2,0,0,0],[s2,coef,0,0,0],[0,0,self.gg,0,0],
+                        [0,0,0,KS_RECT*self.gg,0],[0,0,0,0,KS_RECT*self.gg]]
 
 
-	# 材料を表す文字列を返す
-	def toString(self):
-		return 'Material\t'+self.label.toString(10)+'\t'+ \
-					self.ee+'\t'+self.nu+'\t'+self.gg+'\t'+self.dens+'\t'+ \
-					self.hCon+'\t'+self.sHeat
+    # 材料を表す文字列を返す
+    def toString(self):
+        return 'Material\t'+self.label.toString(10)+'\t'+ \
+                    self.ee+'\t'+self.nu+'\t'+self.gg+'\t'+self.dens+'\t'+ \
+                    self.hCon+'\t'+self.sHeat
 
 
 #--------------------------------------------------------------------#
@@ -77,13 +77,13 @@ class Material:
 # label - パラメータ番号
 # thickness - 厚さ
 class ShellParameter:
-	def __init__(self,label,thickness):
-		self.label=label
-		self.thickness=thickness
+    def __init__(self,label,thickness):
+        self.label=label
+        self.thickness=thickness
 
-	# シェルパラメータを表す文字列を返す
-	def toString(self):
-		return 'ShellParameter\t'+self.label.toString(10)+'\t'+self.thickness
+    # シェルパラメータを表す文字列を返す
+    def toString(self):
+        return 'ShellParameter\t'+self.label.toString(10)+'\t'+self.thickness
 
 #--------------------------------------------------------------------#
 # 梁パラメータ（円形断面）
@@ -91,17 +91,17 @@ class ShellParameter:
 # type - 断面種類
 # ss - データ文字列
 class BarParameter:
-	def __init__(self,label,type,ss):
-		self.label=label
-		self.type=type
-		tp=type.toLowerCase()
-		if tp=='circle':
-			self.section=CircleSection(ss)
-		elif tp=='rectangle':
-			self.section=RectSection(ss)
+    def __init__(self,label,type,ss):
+        self.label=label
+        self.type=type
+        tp=type.toLowerCase()
+        if tp=='circle':
+            self.section=CircleSection(ss)
+        elif tp=='rectangle':
+            self.section=RectSection(ss)
 
-	# 梁パラメータを表す文字列を返す
-	def toString(self):
-		return 'BarParameter\t'+self.label.toString(10)+'\t'+self.type+'\t'+ \
-					self.section.toString()
+    # 梁パラメータを表す文字列を返す
+    def toString(self):
+        return 'BarParameter\t'+self.label.toString(10)+'\t'+self.type+'\t'+ \
+                    self.section.toString()
 
