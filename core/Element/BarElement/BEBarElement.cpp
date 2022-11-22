e11b3cb6b8e701ad8c33b8cf7c454dc98429977f
sasaco <sasaicco@gmail.com>
2022-11-22 18:08:44 +0900

    o

--- core/Element/BarElement/BEBarElement.cpp
+++ core/Element/BarElement/BEBarElement.cpp
@@ -2,7 +2,15 @@
 
 class BEBarElement : public BarElement {
 
+private:
+
+public:
+    BEBarElement(int label, int material, int param, , vector<int> nodes, Vector3 axis);
+    string getName();
+
+
 };
+
 //--------------------------------------------------------------------//
 // Bernoulli-Euler梁要素
 // label - 要素ラベル
@@ -10,27 +18,46 @@ class BEBarElement : public BarElement {
 // param - 梁パラメータのインデックス
 // nodes - 節点番号
 // axis - 断面基準方向ベクトル
-var BEBarElement = function(label, material, param, nodes, axis) {
-    BarElement.call(this, label, material, param, nodes, axis);
+BEBarElement::BEBarElement(int label, int material, int param, , vector<int> nodes, Vector3 axis) :
+    BarElement.call(this, label, material, param, nodes, axis){
 };
 
+
 // 要素境界名称を返す
-BEBarElement.prototype.getName = function() {
-    return 'BEBarElement';
+string BEBarElement::getName() {
+    return "BEBarElement";
 };
+
 
 // 剛性マトリックスの梁曲げ成分を返す
 // l - 要素長さ
 // material - 材料
 // sect - 梁断面パラメータ
-BEBarElement.prototype.stiffBend = function(l, material, sect) {
-    var kc = material.ee / (l * l * l), kcy = kc * sect.iy, kcz = kc * sect.iz;
-    var kcy12 = 12 * kcy, kcy6l = 6 * kcy * l, kcyll = kcy * l * l;
-    var kcz12 = 12 * kcz, kcz6l = 6 * kcz * l, kczll = kcz * l * l;
-    return [[kcy12, kcy6l, -kcy12, kcy6l], [kcy6l, 4 * kcyll, -kcy6l, 2 * kcyll],
-        [-kcy12, -kcy6l, kcy12, -kcy6l], [kcy6l, 2 * kcyll, -kcy6l, 4 * kcyll],
-        [kcz12, -kcz6l, -kcz12, -kcz6l], [-kcz6l, 4 * kczll, kcz6l, 2 * kczll],
-        [-kcz12, kcz6l, kcz12, kcz6l], [-kcz6l, 2 * kczll, kcz6l, 4 * kczll]];
+vector<vector<double>> BEBarElement::stiffBend(double l, Material material, Section sect) {
+
+    double kc = material.ee / (l * l * l);
+    double kcy = kc * sect.iy;
+    double kcz = kc * sect.iz;
+
+    double kcy12 = 12 * kcy;
+    double kcy6l = 6 * kcy * l;
+    double kcyll = kcy * l * l;
+    double kcz12 = 12 * kcz;
+    double kcz6l = 6 * kcz * l;
+    double kczll = kcz * l * l;
+
+    vector<vector<double>> result = {
+        {kcy12, kcy6l, -kcy12, kcy6l}, 
+        {kcy6l, 4 * kcyll, -kcy6l, 2 * kcyll},
+        {-kcy12, -kcy6l, kcy12, -kcy6l}, 
+        {kcy6l, 2 * kcyll, -kcy6l, 4 * kcyll},
+        {kcz12, -kcz6l, -kcz12, -kcz6l}, 
+        {-kcz6l, 4 * kczll, kcz6l, 2 * kczll},
+        {-kcz12, kcz6l, kcz12, kcz6l}, 
+        {-kcz6l, 2 * kczll, kcz6l, 4 * kczll}
+    };
+
+    return result;
 };
 
 
--- core/Element/BarElement/BarElement.h
+++ core/Element/BarElement/BarElement.h
@@ -1,1 +1,31 @@
 #pragma once
+#include "FElement.h"
+#include "Vector3.h"
+
+
+class BarElement : public FElement {
+
+private:
+    double I_YMZ[4] = { 1, 5, 7, 11 };	// y軸方向,z軸周り成分のインデックス
+    double I_ZMY[4] = { 2, 4, 8, 10 };	// z軸方向,y軸周り成分のインデックス
+
+    int param;
+    bool isBar;
+    Vector3 axis;
+
+public:
+    BarElement(int label, int material, int param, vector<int> nodes, Vector3 axis);
+
+    vector<vector<double>> stiffnessMatrix(vector<FENode> p, Material material, Section sect);
+
+    vector<vector<double>> gradMatrix(vector<FENode> p, double coef, Section sect);
+
+    vector<vector<double>> geomStiffnessMatrix(vector<FENode> p, vector<BoundaryCondition> u, Material material, Section sect);
+
+    vector<vector<double>> strainStress(vector<FENode> p, vector<BoundaryCondition> u, Material material, Section sect);
+
+    vector<vector<double>> elementStrainStress(vector<FENode> p, vector<BoundaryCondition> u, Material material, Section sect);
+
+    string toString(vector< Material> materials, vector< BarParameter> params, vector<FENode> p);
+
+};
