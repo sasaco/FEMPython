ad4d94d456c82cdb75e012a7591d7cda9dd0ef4c
sasaco <sasaicco@gmail.com>
2022-11-22 22:43:14 +0900

    TBarElement も作成

--- core/TBarElement.h
+++ core/TBarElement.h
@@ -1,1 +1,20 @@
 ﻿#pragma once
+#include "BarElement.h"
+
+class TBarElement : public BarElement {
+
+private:
+
+
+public:
+    TBarElement(int label, int material, int param, double* nodes, double* axis);
+
+    string getName();
+
+    vector<vector<double>> stiffBend(double l, Material material, Section sect);
+
+    vector<vector<double>> bendCurveShare(vector<double> v, double l, Material material, Section sect);
+
+    vector<vector<double>> massMatrix(vector<FENode> p, double dens, Section sect);
+
+};
