bfc2c1207f01aa170dcf414db5b944c4170551fa
sasaco <sasaicco@gmail.com>
2022-12-30 14:08:59 +0900

    python に戻す

--- tests/test_001.py
+++ tests/test_001.py
@@ -1,10 +1,14 @@
 import pytest
+import sys 
+import os
 
 from src.FemMain import initModel
 
 
 def test_sampleBeamHexa1():
-    model = initModel('tests/example_files/beam/sampleBeamHexa1.fem')
+    file_path = sys.path.append(os.path.abspath(os.path.dirname(os.path.abspath(__file__))
+                + "/../examples/beam/sampleBeamHexa1.fem"))
+    model = initModel(file_path)
     model.calculate()
     assert True
 
