282db20aede5d5de4c0006d44a10b8fcf12b6a18
sasaco <sasaicco@gmail.com>
2023-01-10 17:39:33 +0900

    CW用に

--- tests/test_001.py
+++ tests/test_001.py
@@ -6,12 +6,9 @@ def test_sampleBeamHexa1():
 
     fileName = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + '/../examples/beam/sampleBeamHexa1.fem')
     f = open(fileName, encoding="utf-8")
-    fstr = f.read()  # ファイル終端まで全て読んだデータを返す
+    s = f.read()  # ファイル終端まで全て読んだデータを返す
     f.close()
 
     # ここに pybind11 をつかって c++ と連携する処理
-    ###
-    ###
-    ###
-    ###
+    FemDataModel::readFemModel(s); ## この関数に string s を渡して演算をさせたい
 
