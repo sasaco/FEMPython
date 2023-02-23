1bad8db7f01fa1ba2a8ac7c481654598c94432e9
sasaco <sasaicco@gmail.com>
2023-02-23 15:56:31 +0900

    python is back

--- tests/test_001.py
+++ tests/test_001.py
@@ -1,25 +1,24 @@
 import pytest
+import conftest
 import sys
 import os
 
-from src.FemMain import initModel
+from FemMain import initModel
 
 def test_sampleBeamHexa1():
 
-    fileName =  os.path.abspath(os.path.dirname(os.path.abspath(__file__))
-                + "/../examples")
-    fileName += '/beam/sampleBeamHexa1.fem'
+    fileName = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) 
+                + '/examples/beam/sampleBeamHexa1.fem')
 
     f = open(fileName, encoding="utf-8")
-    fstr = f.read()  # ファイル終端まで全て読んだデータを返す
+    fstr = f.read()  
     f.close()
 
     model = initModel(fstr)
     model.calculate()
 
-
     assert True
 
 
-
-
+if __name__ == "__main__":
+    test_sampleBeamHexa1()
