10a10507731e1795ab4e365d66bcf7e555c86c09
sasaco <sasaicco@gmail.com>
2023-03-03 22:39:34 +0900

    フォルダ構成

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+import sys 
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '../core/bin'))
 import core
 
 def initModel(s: str):
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,4 +2,3 @@ import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + "/../src/"))
-sys.path.append(os.path.join(os.path.dirname(__file__), '../core'))
