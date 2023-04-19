b6b68e42187d11f0da66aa63eb4fa41105a1a2d9
sasaco <sasaicco@gmail.com>
2023-04-19 21:49:09 +0900

    計算結果の表示

--- src/main.py
+++ src/main.py
@@ -37,42 +37,5 @@ class FEMPython(_dataManager):
 
 
     def vtk(self):
-        import numpy as np
-        from pyevtk.hl import imageToVTK
-
-        ix = 128
-        jx = 128
-        kx = 128
-
-        xmin = 0.
-        xmax = 1.
-        ymin = 0.
-        ymax = 1.
-        zmin = 0.
-        zmax = 1.
-
-        dx = (xmax-xmin)/ix
-        dy = (ymax-ymin)/jx
-        dz = (zmax-zmin)/kx
-
-        x = xmin + 0.5*dx + np.arange(ix)*dx
-        y = ymin + 0.5*dy + np.arange(jx)*dy
-        z = zmin + 0.5*dz + np.arange(kx)*dz
-
-        xx,yy,zz = np.meshgrid(x,y,z)
-
-        ds = 0.1
-
-        x0 = (xmax+xmin)/2.
-        y0 = (ymax+ymin)/2.
-        z0 = (zmax+zmin)/2.
-
-        amp = 1.0
-
-        rr = (xx-x0)**2+(yy-y0)**2+(zz-z0)**2
-        qq = amp*np.exp(-rr/(2.*ds**2))
-
-        imageToVTK('./test',pointData={'quantity':qq})
-
         return "OKS"
 
--- tests/test_001.py
+++ tests/test_001.py
@@ -12,12 +12,9 @@ def test_sampleBeamHexa1():
     # src 関数を呼ぶテスト
     fem = readTestData(s)
     result = fem.calculate()
+    ed = result.ELEMENT_DATA
     vtk: str = fem.vtk()
     
-    ## 計算結果を書き込む
-    #fileName = os.path.join(filePath, os.path.splitext(os.path.basename(fileName))[0]) + '.vtk'
-    #with open(fileName, 'w') as f:
-    #    f.write(vtk)
 
     assert True
 
