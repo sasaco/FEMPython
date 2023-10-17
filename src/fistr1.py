5f18b1ecaafbe9327ec483f2f205992e35149181
sasaco <sasaicco@gmail.com>
2023-10-17 22:44:11 +0900

    fin

--- src/fistr1.py
+++ src/fistr1.py
@@ -1,7 +1,7 @@
 from subprocess import run, PIPE
 
 
-run("/workspaces/FEMPython/FrontISTR/bin/fistr1",
+run("/usr/bin/fistr1",
     shell=True, 
     cwd=r"/workspaces/FEMPython/tests/01_elastic_hinge/")
 
