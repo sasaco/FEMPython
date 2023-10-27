56efb2c5056c41b48b21755dd0959a4e49832f5c
sasaco <sasaicco@gmail.com>
2023-10-27 11:32:27 +0900

    バー要素のサンプルを追加

--- tests/test_001.py
+++ tests/test_001.py
@@ -1,1 +1,10 @@
-print('test!!!!')
+from subprocess import run, PIPE
+
+
+run("/workspaces/FEMPython/FrontISTR/bin/fistr1",
+    shell=True, 
+    cwd=r"/workspaces/FEMPython/tests/101_bar_test/")
+
+
+print("aaa")
+
